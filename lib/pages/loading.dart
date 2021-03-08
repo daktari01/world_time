d0f980f4a828eff75a6c2b6aa5d3529c55898d16
James Dindi <dindijjames@gmail.com>
2021-03-08 19:48:12 +0300

    World time API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,19 +9,25 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  void getTime() async {
 
-    final response = await http.get(Uri.https('jsonplaceholder.typicode.com', 'todos/1'));
+    final response = await http.get(Uri.https('worldtimeapi.org', 'api/timezone/Africa/Nairobi'));
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    // print(data);
 
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+
+    DateTime now = DateTime.parse(datetime);
+    print('Before -> $now');
+    now = now.add(Duration(hours: int.parse(offset)));
+    print('After -> $now');
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
